df45cb849b5d418c4b3460389b1c7ac3a07c5b3c
Max <Max_Groenenboom@hotmail.com>
2016-05-24 14:12:44 +0200

    Created support for AJAX.
    Also added support for an inconsistency in Github and some general fixes.

--- GithubPRLink.user.js
+++ GithubPRLink.user.js
@@ -3,7 +3,7 @@
 // @namespace   MG
 // @description Adds a hyperlink to the head branch.
 // @include     *github.com/*
-// @version     1.0.1
+// @version     1.0.2
 // @grant       none
 // @updateURL   https://github.com/MaxGroenenboom/userscript-github-pr-link/raw/master/GithubPRLink.meta.js
 // @downloadURL https://github.com/MaxGroenenboom/userscript-github-pr-link/raw/master/GithubPRLink.user.js
@@ -14,20 +14,36 @@ function createLinks() {
 	var baseLink = location.protocol + "//" + location.hostname + "/";
 	for (var i = 0; i < fields.length; i++) {
 		var field = fields[i];
+
+		// Only add a link if it hasn't been added already.
+		if (field.classList.contains("mg-hyperlink-added")) {
+			continue;
+		}
+		field.classList.add("mg-hyperlink-added");
+
+		// If the span has no title field, use the inner span.
+		// Necessary because of an inconsistency in Github.
+		if (!field.title) {
+			field = field.childNodes[0];
+		}
+
+		// Create the href to the reference.
 		var url = baseLink + field.title;
 		var colonIndex = url.lastIndexOf(":");
 		var branch = url.substring(colonIndex + 1);
 		url = url.substring(0, colonIndex);
 		url += "/tree/" + branch;
 
+		// Create the element and wrap the original contents inside it.
 		var newElement = document.createElement("a");
 		newElement.href = url;
 		var fieldElements = field.childNodes;
 		newElement.innerHTML = field.innerHTML;
 		field.innerHTML = newElement.outerHTML;
 	}
+
+	// Retry in 250 milliseconds.
+	setTimeout(createLinks, 250);
 }
 
-onload = function() {
-	setTimeout(createLinks, 10);
-};
+createLinks();
